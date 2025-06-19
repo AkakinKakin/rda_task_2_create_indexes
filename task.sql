ba27bbb8700f13051f16f25bba4cf1a667da5e3e
Eldar <eldardemchenko19@mail.com>
2025-06-19 04:13:52 +0300

    fixed0.3

--- task.sql
+++ task.sql
@@ -1,13 +1,6 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB; 
-DROP INDEX idx_products_name ON Products;
-CREATE INDEX idx_products_name ON Products(Name);
 
-DROP INDEX idx_customers_email ON Customers;
-CREATE INDEX idx_customers_email ON Customers(Email);
+CREATE INDEX Name ON Products(Name);
 
-DROP INDEX idx_orders_customerid ON Orders;
-CREATE INDEX idx_orders_customerid ON Orders(CustomerID);
-
-DROP INDEX idx_orderitems_orderid ON OrderItems;
-CREATE INDEX idx_orderitems_orderid ON OrderItems(OrderID);
+CREATE INDEX Email ON Customers(Email);
